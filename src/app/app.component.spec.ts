0d89a17a77971ca165d8ecf245188be579e3644a
Lebogang Molapo <lmolapo021@student.wethinkcode.co.za>
2022-08-11 21:35:06 +0200

    Removed class properties and tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,5 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-testing');
-  });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ang-testing app is running!');
-  });
 });
